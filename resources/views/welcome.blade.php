19a5ead704419051a1261a80276b2b5e106bd5b4
TheSoloGod <kienaof94@gmail.com>
2019-12-18 01:03:28 +0700

    remove sth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,13 +14,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
-    <!-- Booostrap JS -->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="/css/app.css">
 
